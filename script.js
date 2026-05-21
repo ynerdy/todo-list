bf7ca2ae9a3c675ae92bee1457144229732bf160
ynerdy <180704232+ynerdy@users.noreply.github.com>
2026-05-21 12:06:47 -0500

    i just added a comment on anon/named functions on js ngl

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let taskInput = document.getElementById("input-area");
 let addBtn = document.getElementById("add-task-button");
 let todoList = document.getElementById("todo-list");
 
+// just so u know there are differences between an "anonymous function" and a "named function"
 addBtn.addEventListener("click", function() {
     // This is the "brain" of your button.
     // Anything you type in here will happen ONLY when the button is clicked!
     console.log("Boom! The button was clicked!");
     // 1. Grab the value (text) from the input box
     let userText = taskInput.value;
-    
     // 2. Print that specific text to the console
     console.log(userText);
 });
