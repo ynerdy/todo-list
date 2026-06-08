111b5889a4dcfb05a8a09e677796f6b79b087805
ynerdy <180704232+ynerdy@users.noreply.github.com>
2026-06-08 15:54:51 -0500

    so just removing a printing line on the handleAddtask function made it so we have the GOOD old behaviour where the brwoser on the console basically just "accumulates" the same thing on the little bubble on the left instead of filling the whole screen or console with the same message (its not more elegant this way yk?) like fr

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function handleAddTask() {
 
     // The guard checks if it's NOT empty
     if (userText !== "") {
-        console.log("Boom! Valid task detected!");
+        
         console.log("User typed:", userText);
 
         let newLI = document.createElement("li");
